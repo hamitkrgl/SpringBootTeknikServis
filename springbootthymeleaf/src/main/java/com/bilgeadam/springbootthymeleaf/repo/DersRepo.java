8a8fab1c9d55aeb2eceeea79d2b4e9655cb3c680
Hamit Köroğlu <hamitkrgl@icloud.com>
2022-07-23 00:19:46 +0300

    Delete springbootthymeleaf directory

--- springbootthymeleaf/src/main/java/com/bilgeadam/springbootthymeleaf/repo/DersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bilgeadam.springbootthymeleaf.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bilgeadam.springbootthymeleaf.model.Ders;
-
-public interface DersRepo extends JpaRepository<Ders, Long> {
-
-}
